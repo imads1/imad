91379b244fc09f2c9bb7c33e8eee3e90164050c6
imads1 <ddido7952@gmail.com>
2026-04-22 01:10:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -217,7 +217,7 @@ def home():
 
 def run_api():
     print("🌐 بدء تشغيل API...")
-    app.run(host='0.0.0.0', port=7005, debug=False)
+    app.run(host='0.0.0.0', debug=False)
 
 def AuTo_ResTartinG():
     time.sleep(6 * 60 * 60)
